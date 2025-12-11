9b90266f5240686cb349019d8e4f14d315a0c57b
Leandro Dantas <leandrodantas659@gmail.com>
2025-12-11 08:56:23 -0300

    ✨ update authentication layout; move dropdown menu inside authentication check and simplify login route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Livewire\Login;
 use App\Livewire\PrizeDrawManagement\Index;
 use Illuminate\Support\Facades\Route;
 
-Route::get('prize-draw/login', Login::class)->name('login');
+Route::get('login', Login::class)->name('login');
 
 Route::prefix('prize-draw')
     ->name('prize-draw.')
